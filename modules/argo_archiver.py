aa2ac7e10610298cccebc608db300bf8e46fa549
Dean Hudek <hudek.dean@gmail.com>
2023-07-04 09:54:21 +0200

    warning and critical messages return correctly

--- modules/argo_archiver.py
+++ modules/argo_archiver.py
@@ -12,35 +12,42 @@ def process_files(hostname):
     config = configparser.ConfigParser()
     config.read(f'/etc/argo-ams-consumer/ams-consumer-{hostname}.conf')
 
-    directory = config.get("Output", "Directory")
+    try:
+        directory = config.get("Output", "Directory")
+    
+        files = os.listdir(directory)
 
-    files = os.listdir(directory)
+        sorted_file_paths = sorted(files, key=lambda file: datetime.strptime(
+            file.split('_')[-1].split('.')[0], "%Y-%m-%d"))
 
-    sorted_file_paths = sorted(files, key=lambda file: datetime.strptime(
-        file.split('_')[-1].split('.')[0], "%Y-%m-%d"))
+        # Checks if a file with today's stamp exists in the directory
+        todays_date = datetime.today().strftime("%Y-%m-%d")
+        todays_path = f'argo-consumer_log_{todays_date}.avro'
 
-    # Checks if file with today's stamp exists in directory
-    todays_date = datetime.today().strftime("%Y-%m-%d")
-    todays_path = f'argo-consumer_log_{todays_date}.avro'
+        if todays_path in sorted_file_paths:
+            todays_stats = os.stat(f"/var/lib/argo-ams-consumer/{hostname}/{todays_path}")
 
-    if todays_path in sorted_file_paths:
-        todays_stats = os.stat(f"/var/lib/argo-ams-consumer/{hostname}/{todays_path}")
+            modify_time = datetime.fromtimestamp(todays_stats.st_mtime)
+            time_two_hrs_ago = datetime.now() - timedelta(hours=2, minutes=0)
 
-        modify_time = datetime.fromtimestamp(todays_stats.st_mtime)
-        time_two_hrs_ago = datetime.now() - timedelta(hours=2, minutes=0)
+            # Checks if the file has been modified in the last two hours
+            if modify_time < time_two_hrs_ago:
+                nagios.setCode(nagios.WARNING)
+                nagios.writeWarningMessage("Today's file " + todays_path +
+                    f" in {hostname.upper()} directory hasn't been modified in the last 2 hours.")
 
-        # Checks if the file has been modified in last two hours
-        if modify_time > time_two_hrs_ago:
-            print(nagios.getMsg())
         else:
-            nagios.setCode(nagios.WARNING)
-            print("Today's file " + todays_path +
-                  f" in {hostname.upper()} directory hasn't been modified if last 2 hours.")
+            nagios.setCode(nagios.CRITICAL)
+            nagios.writeCriticalMessage(
+                "Today's file " + todays_path + f" in {hostname.upper()} directory is missing for today.")
 
-    else:
-        nagios.setCode(nagios.CRITICAL)
-        print("Today's file " + todays_path + f" in {hostname.upper()} directory is missing for today.")
-        raise SystemExit(nagios.getCode())
+    except:
+            nagios.setCode(nagios.CRITICAL)
+            nagios.writeCriticalMessage("Directory does not exist.")
+
+    print(nagios.getMsg())
+    raise SystemExit(nagios.getCode())
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -51,7 +58,6 @@ def main():
     hostname = cmd_options.hostname
 
     process_files(hostname)
-
 
 if __name__ == "__main__":
     main()
